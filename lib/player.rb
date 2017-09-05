1a516e8b22f23156fc42cc6bcad2e1984abbd84e
Tyjoo26 <Tyjoo21@gmail.com>
2017-09-04 22:00:50 -0600

    Converted input for validation, working through validation method

--- lib/player.rb
+++ lib/player.rb
@@ -10,7 +10,27 @@ class Player
     @board = GameBoard.new
   end
 
+  def convert_input_for_validation_two_boat
+    user_input = gets.chomp
+    user_input = user_input.split(" ")
+  end
 
+  def convert_to_space_1
+    convert_input_for_validation[0]
+  end
+
+  def convert_to_space_2
+    conver_input_for_validation[1]
+  end
+
+  def validate_two_boat_coord
+    if @board.cant_place_diagonally_two(convert_to_space_1, convert_to_space_2) == false
+      invalid_coordinate_message
+    elsif @board.cant_wrap_horizontally_two_unit(convert_to_space_1, convert_to_space_2)
+      invalid_coordinate_message
+    elsif @board.cant_wrap_vertically_two_unit(convert_to_space_1, convert_to_space_2)
+    elsif @board.check_nil(convert_to_space_1, convert_to_space_2)
+  end
 
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -30,4 +30,5 @@ class PlayerTest < MiniTest::Test
   end
 
 
+
 end
